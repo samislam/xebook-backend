c24a226a24e07797211ae7835d1eb3bbdc24beed
Islam Yamor <islamyamorz@gmail.com>
2026-04-03 09:37:00 +0300

    corrected empty query string case

--- src/modules/users/dto/list-users-query.dto.ts
+++ src/modules/users/dto/list-users-query.dto.ts
@@ -1,9 +1,10 @@
 import { z } from 'zod'
 import { createZodDto } from 'nestjs-zod'
+import { stringToBoolean } from '@/common/utils/zod'
 import { paginationQuerySchema } from '@/common/dtos/pagination-query.dto'
 
 export const listUsersQuerySchema = paginationQuerySchema.extend({
-  isActive: z.boolean().optional(),
+  isActive: z.preprocess(stringToBoolean, z.boolean().optional()),
 })
 
 export class ListUsersQueryDto extends createZodDto(listUsersQuerySchema) {}
